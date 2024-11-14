b24e5ffba0703f1ec992f412fbe0e5b00ee0db82
Alexandre Caron <alexandrecaron438@pm.me>
2024-11-14 16:27:52 -0500

    Modified content and added basic CSS

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+      <header>
+          <nav className="nav">
+              <img src={logo} alt="React logo" className="logo" />
+              <ul className="nav-items">
+                  <li>Pricing</li>
+                  <li>About</li>
+                  <li>Contact</li>
+              </ul>
+          </nav>
+      </header>
+  )
+}
+
+function MainContent() {
+  return (
+      <div>
+          <h1>Reasons I'm excited to learn React</h1>
+          <ol>
+              <li>It's a popular library, so I'll be 
+              able to fit in with the cool kids!</li>
+              <li>I'm more likely to get a job as a developer
+              if I know React</li>
+          </ol>
+      </div>
+  )
+}
+
+function Footer() {
+  return (
+      <footer>
+          <small>© 2024 Caron development. All rights reserved.</small>
+      </footer>
+  )
+}
+
 function App() {
   return (
     <div>
-      <img src={logo} alt="React Logo" width="40px"></img>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Wast first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of entreprise apps, including mobile apps</li>
-      </ul>
+        <Header />
+        <MainContent />
+        <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
